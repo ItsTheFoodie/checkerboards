2ebd0e038fe7b3ef11721230d411c4e5a6a9933d
ItsTheFoodie <firdausftoure@gmail.com>
2026-06-28 18:49:11 +0000

    first committtt

--- src/App.js
+++ src/App.js
@@ -1,13 +1,98 @@
-function Square() {
-  return <button className="square"></button>;
+function Square({value}) {
+  return <button className="square">{value}</button>;
 }
 
 export default function Board() {
   return (
-    <>
+    <> 
+    <div className="board-row">
       <Square />
       <Square />
       <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      
+    </div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      
+    </div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      
+    </div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      
+    </div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      
+    </div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      
+    </div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      
+    </div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      
+    </div>
     </>
   );
 }
